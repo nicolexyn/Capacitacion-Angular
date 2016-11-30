ec3d43fcaa49efad6ca0f4b229f757a126513857
Nicolas Olexyn <nicolexyn@gmail.com>
2016-11-29 23:06:32 -0300

    update files

---
+++ node.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const express = require('express')
+const app = express()
+const path = require('path')
+app.use('/', express.static(__dirname + '/'));
+app.get('/', function(req, res) {
+        res.sendFile(path.join(__dirname + '/index.html'));
+    });
+app.listen(3000, () => {
+        console.log(`Node corriendo en un http://localhost:3000`)
+    })
+module.exports = app
